4350a10127e808b619a2fcf394dae112fcdfe0b5
martinsareto <martin.kiprotich@student.moringaschool.com>
2022-07-30 23:12:34 +0300

    updates

--- stuttgartautos/src/About.js
+++ stuttgartautos/src/About.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import {pic1} from './image/benz.jpg'
+import pic1 from './image/benz.jpg';
 
 
 
@@ -9,7 +9,7 @@ export default function About(){
     return(
         
 <div id="about">
-  <img src={pic1}/>
+  <img src={pic1} className='pic1'/>
 <p className="p1"> We offer a complete range of automotive services from repairing slight bumper scrapes to handling large insurance claims. After many years serving our customers we pride ourselves in giving every case from small to large the upmost attention and care.</p>
 </div>
     )
