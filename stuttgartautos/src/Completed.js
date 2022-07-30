f9e314af125ac02825107fd3d9afc931a63152cf
martinsareto <martin.kiprotich@student.moringaschool.com>
2022-07-31 01:20:31 +0300

    removing white spaces

--- stuttgartautos/src/Completed.js
+++ stuttgartautos/src/Completed.js
@@ -1,8 +1,6 @@
 import { set } from "date-fns";
 import React,{useState,useEffect} from "react";
 import Book from "./Book";
-
-
 export default function Completed(){
     const [cars, setCars] = useState([]);
     useEffect(() => {
